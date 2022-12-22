343ac84f2e441dee4696346edd2519a312030658
Nicolai Sattler <nicolaisattler1990@gmail.com>
2022-12-22 20:37:09 +0100

    added unit tests for Rook

--- Chess.Test/Pieces/RookTests.cs
+++ Chess.Test/Pieces/RookTests.cs
@@ -17,7 +17,7 @@ public class RookTests
     }
 
     [TestMethod]
-    public void  Name()
+    public void WhiteAttackRange_A1_ShouldBeValid()
     {
         //Arrange
         _sut = new() { Position = new(File.A, 1), Color = Color.White };
@@ -32,5 +32,39 @@ public class RookTests
         result.ShouldContain(s => s == new Square(File.B, 1));
         result.ShouldContain(s => s == new Square(File.E, 1));
         result.ShouldContain(s => s == new Square(File.H, 1));
+    }
+
+    [TestMethod]
+    public void BlackAttackRange_A8_ShouldBeValid()
+    {
+        //Arrange
+        _sut = new() { Position = new(File.A, 8), Color = Color.Black };
+
+        //Act
+        var result = _sut.GetAttackRange();
+
+        //Assert
+        result.ShouldContain(s => s == new Square(File.A, 7));
+        result.ShouldContain(s => s == new Square(File.A, 5));
+        result.ShouldContain(s => s == new Square(File.A, 1));
+        result.ShouldContain(s => s == new Square(File.B, 8));
+        result.ShouldContain(s => s == new Square(File.E, 8));
+        result.ShouldContain(s => s == new Square(File.H, 8));
+    }
+
+    [TestMethod]
+    public void WhiteAttackRange_D4_ShouldBeValid()
+    {
+        //Arrange
+        _sut = new() { Position = new(File.D, 4), Color = Color.White };
+
+        //Act
+        var result = _sut.GetAttackRange();
+
+        //Assert
+        result.ShouldContain(s => s == new Square(File.D, 1));
+        result.ShouldContain(s => s == new Square(File.D, 8));
+        result.ShouldContain(s => s == new Square(File.A, 4));
+        result.ShouldContain(s => s == new Square(File.H, 4));
     }
 }
