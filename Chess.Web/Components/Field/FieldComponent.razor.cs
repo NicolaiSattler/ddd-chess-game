ea921abb2803c5a171ebc17cd26401d130b56f34
Nicolai Sattler <nicolaisattler1990@gmail.com>
2023-04-07 14:12:48 +0200

    added svg pieces

--- Chess.Web/Components/Field/FieldComponent.razor.cs
+++ Chess.Web/Components/Field/FieldComponent.razor.cs
@@ -6,18 +6,37 @@ namespace Chess.Web.Components.Field;
 
 public partial class FieldComponent
 {
+    private BackgroundColour _colour;
+
+    private string HtmlClasses => _colour == BackgroundColour.Dark
+                               ? "dark-field field"
+                               : "light-field field";
+    private bool ShowRank => this.File == File.A;
+    private bool ShowFile => this.Rank == 8;
+
     [Parameter, EditorRequired]
     public int Rank { get; set; }
 
     [Parameter, EditorRequired]
     public File File { get; set; }
 
-    [Parameter, EditorRequired]
-    public BackgroundColour Colour { get; set; }
+    protected override void OnInitialized()
+    {
+        _colour = SelectColour();
 
-    public string HtmlClasses => Colour == BackgroundColour.Dark
-                              ? "dark-field field"
-                              : "light-field field";
+        base.OnInitialized();
+    }
+
+    private BackgroundColour SelectColour()
+    {
+        var file = (int)this.File;
+
+        return (Rank % 2) > 0
+                ? (file % 2) > 0
+                     ? BackgroundColour.Light : BackgroundColour.Dark
+                : (file % 2) == 1
+                     ? BackgroundColour.Dark : BackgroundColour.Light;
+    }
 }
 
 public enum BackgroundColour
