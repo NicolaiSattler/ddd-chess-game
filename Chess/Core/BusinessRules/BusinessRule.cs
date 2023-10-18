c801af1f3eea03fdcb8e9a642748ce41ba6b2945
Nicolai Sattler <nicolaisattler1990@gmail.com>
2023-10-18 13:17:48 +0200

    removed AndBusinessRule

--- Chess/Core/BusinessRules/BusinessRule.cs
+++ Chess/Core/BusinessRules/BusinessRule.cs
@@ -5,13 +5,4 @@ namespace Chess.Core.BusinessRules;
 public abstract class BusinessRule
 {
     public abstract IEnumerable<BusinessRuleViolation> CheckRule();
-
-    public BusinessRule And(BusinessRule rule)
-    {
-        return new AndBusinessRule(this, rule);
-    }
-    public static BusinessRule operator &(BusinessRule firstRule, BusinessRule secondRule)
-    {
-        return new AndBusinessRule(firstRule, secondRule);
-    }
 }
