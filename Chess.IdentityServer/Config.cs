dd6898ad06159db6d8029db099794bdddac4d228
Nicolai Sattler <nicolaisattler1990@gmail.com>
2023-10-20 21:57:39 +0200

    init IdentityServer + PlayerAPI projects

---
+++ Chess.IdentityServer/Config.cs
@@ -0,0 +1,33 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace Chess.IdentityServer;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
+    {
+        new IdentityResources.OpenId()
+    };
+
+    public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
+    {
+        new ApiScope(name: "PlayerApi", displayName: "Chess.PlayerApi")
+    };
+
+    public static IEnumerable<Client> Clients => new Client[]
+    {
+        new()
+        {
+            ClientId = "client", //Replace with id of Chess.Web (has to be determined)
+            // no interactive user, use the clientid/secret for authentication
+            AllowedGrantTypes = GrantTypes.ClientCredentials,
+            ClientSecrets =
+            {
+                new("secret".Sha256())
+            },
+            AllowedScopes = { "PlayerApi" }
+
+            //TODO: https://docs.duendesoftware.com/identityserver/v6/quickstarts/1_client_credentials/#add-jwt-bearer-authentication
+        }
+    };
+}
