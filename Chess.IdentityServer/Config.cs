a8e2eba8d336ba0bb3fffaf830fe959bef068d52
Nicolai Sattler <nicolaisattler1990@gmail.com>
2023-12-20 20:33:59 +0100

    initialized identityserver with UI

--- Chess.IdentityServer/Config.cs
+++ Chess.IdentityServer/Config.cs
@@ -4,30 +4,49 @@ namespace Chess.IdentityServer;
 
 public static class Config
 {
-    public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
-    {
-        new IdentityResources.OpenId()
-    };
-
-    public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
-    {
-        new ApiScope(name: "PlayerApi", displayName: "Chess.PlayerApi")
-    };
-
-    public static IEnumerable<Client> Clients => new Client[]
-    {
-        new()
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
         {
-            ClientId = "client", //Replace with id of Chess.Web (has to be determined)
-            // no interactive user, use the clientid/secret for authentication
-            AllowedGrantTypes = GrantTypes.ClientCredentials,
-            ClientSecrets =
-            {
-                new("secret".Sha256())
-            },
-            AllowedScopes = { "PlayerApi" }
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+        };
 
-            //TODO: https://docs.duendesoftware.com/identityserver/v6/quickstarts/1_client_credentials/#add-jwt-bearer-authentication
-        }
-    };
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+        {
+            new ApiScope("scope1"),
+            new ApiScope("scope2"),
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[]
+        {
+            // m2m client credentials flow client
+            new Client
+            {
+                ClientId = "m2m.client",
+                ClientName = "Client Credentials Client",
+
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+
+                AllowedScopes = { "scope1" }
+            },
+
+            // interactive client using code flow + pkce
+            new Client
+            {
+                ClientId = "interactive",
+                ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+
+                AllowedGrantTypes = GrantTypes.Code,
+
+                RedirectUris = { "https://localhost:44300/signin-oidc" },
+                FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
+                PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+
+                AllowOfflineAccess = true,
+                AllowedScopes = { "openid", "profile", "scope2" }
+            },
+        };
 }
--- Chess.IdentityServer/HostingExtensions.cs
+++ Chess.IdentityServer/HostingExtensions.cs
@@ -1,3 +1,10 @@
+using Duende.IdentityServer;
+using Chess.IdentityServer;
+using Chess.IdentityServer.Pages.Admin.ApiScopes;
+using Chess.IdentityServer.Pages.Admin.Clients;
+using Chess.IdentityServer.Pages.Admin.IdentityScopes;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 namespace Chess.IdentityServer;
@@ -6,39 +13,94 @@ internal static class HostingExtensions
 {
     public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
     {
-        // uncomment if you want to add a UI
-        //builder.Services.AddRazorPages();
+        builder.Services.AddRazorPages();
 
-        builder.Services.AddIdentityServer(options =>
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+        var isBuilder = builder.Services
+            .AddIdentityServer(options =>
             {
-                // https://docs.duendesoftware.com/identityserver/v6/fundamentals/resources/api_scopes#authorization-based-on-scopes
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+
+                // see https://docs.duendesoftware.com/identityserver/v5/fundamentals/resources/
                 options.EmitStaticAudienceClaim = true;
             })
-            .AddInMemoryIdentityResources(Config.IdentityResources)
-            .AddInMemoryApiScopes(Config.ApiScopes)
-            .AddInMemoryClients(Config.Clients);
+            .AddTestUsers(TestUsers.Users)
+            // this adds the config data from DB (clients, resources, CORS)
+            .AddConfigurationStore(options =>
+            {
+                options.ConfigureDbContext = b =>
+                    b.UseSqlite(connectionString, dbOpts => dbOpts.MigrationsAssembly(typeof(Program).Assembly.FullName));
+            })
+            // this is something you will want in production to reduce load on and requests to the DB
+            //.AddConfigurationStoreCache()
+            //
+            // this adds the operational data from DB (codes, tokens, consents)
+            .AddOperationalStore(options =>
+            {
+                options.ConfigureDbContext = b =>
+                    b.UseSqlite(connectionString, dbOpts => dbOpts.MigrationsAssembly(typeof(Program).Assembly.FullName));
+            });
+
+        builder.Services.AddAuthentication()
+            .AddGoogle(options =>
+            {
+                options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+
+                // register your IdentityServer with Google at https://console.developers.google.com
+                // enable the Google+ API
+                // set the redirect URI to https://localhost:5001/signin-google
+                options.ClientId = "copy client ID from Google here";
+                options.ClientSecret = "copy client secret from Google here";
+            });
+
+
+        // this adds the necessary config for the simple admin/config pages
+        {
+            builder.Services.AddAuthorization(options =>
+                options.AddPolicy("admin",
+                    policy => policy.RequireClaim("sub", "1"))
+            );
+
+            builder.Services.Configure<RazorPagesOptions>(options =>
+                options.Conventions.AuthorizeFolder("/Admin", "admin"));
+
+            builder.Services.AddTransient<Chess.IdentityServer.Pages.Portal.ClientRepository>();
+            builder.Services.AddTransient<ClientRepository>();
+            builder.Services.AddTransient<IdentityScopeRepository>();
+            builder.Services.AddTransient<ApiScopeRepository>();
+        }
+
+        // if you want to use server-side sessions: https://blog.duendesoftware.com/posts/20220406_session_management/
+        // then enable it
+        //isBuilder.AddServerSideSessions();
+        //
+        // and put some authorization on the admin/management pages using the same policy created above
+        //builder.Services.Configure<RazorPagesOptions>(options =>
+        //    options.Conventions.AuthorizeFolder("/ServerSideSessions", "admin"));
 
         return builder.Build();
     }
-    
+
     public static WebApplication ConfigurePipeline(this WebApplication app)
-    { 
+    {
         app.UseSerilogRequestLogging();
-    
+
         if (app.Environment.IsDevelopment())
         {
             app.UseDeveloperExceptionPage();
         }
 
-        // uncomment if you want to add a UI
-        //app.UseStaticFiles();
-        //app.UseRouting();
-            
+        app.UseStaticFiles();
+        app.UseRouting();
         app.UseIdentityServer();
+        app.UseAuthorization();
 
-        // uncomment if you want to add a UI
-        //app.UseAuthorization();
-        //app.MapRazorPages().RequireAuthorization();
+        app.MapRazorPages()
+            .RequireAuthorization();
 
         return app;
     }
